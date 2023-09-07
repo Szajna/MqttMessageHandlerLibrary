1241a1b8e468280c8d9b39a4fe449041ece484ca
szajn <szajn@PCMATI>
2023-09-07 12:05:36 +0200

    Add project files.

---
+++ MqttMessageApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using MqttMessageHandlerLibrary;
+using System;
+
+namespace MqttMessageApp
+{
+    internal class Program
+    {
+        private const string BrokerAdress = "broker.hivemq.com";
+
+        static void Main(string[] args)
+        {
+            var mqttMessageSender = new MqttMessageSender(BrokerAdress);
+
+            var mqttMessage = new MqttMessage
+            {
+                Topic = "testTopic",
+                Message = "testMessage"
+            };
+
+            mqttMessageSender.SendMessage(mqttMessage);
+            mqttMessageSender.Disconnect();
+
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ MqttMessageHandlerLibrary/MqttMessageSender.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using uPLibrary.Networking.M2Mqtt;
+using uPLibrary.Networking.M2Mqtt.Exceptions;
+using uPLibrary.Networking.M2Mqtt.Messages;
+
+namespace MqttMessageHandlerLibrary
+{
+    public class MqttMessageSender
+    {
+        private readonly MqttClient mqttClient;
+
+        public MqttMessageSender(string brokerAddress)
+        {
+            if (string.IsNullOrEmpty(brokerAddress))
+            {
+                throw new ArgumentNullException(nameof(brokerAddress), "Broker address cannot be empty.");
+            }
+
+            mqttClient = new MqttClient(brokerAddress);
+            try
+            {
+                mqttClient.Connect(Guid.NewGuid().ToString());
+
+            }
+            catch (MqttConnectionException ex)
+            {
+                Console.WriteLine($"MQTT connection error: {ex.Message}");
+                throw;
+            }
+
+            Console.WriteLine("MQTT client is successfully connected.");
+        }
+
+        public void SendMessage(MqttMessage message)
+        {
+            if (!mqttClient.IsConnected)
+            {
+                Console.WriteLine("MQTT client is not connected.");
+                return;
+            }
+
+            try
+            {
+                mqttClient.Publish(message.Topic, System.Text.Encoding.UTF8.GetBytes(message.Message), MqttMsgBase.QOS_LEVEL_EXACTLY_ONCE, false);
+            }
+            catch(MqttConnectionException ex)
+            {
+                Console.WriteLine($"Publish message failed: {ex.Message}");
+            }
+
+            Console.WriteLine($"MQTT message: {message.Topic}, sent.");
+        }
+
+        public void Disconnect()
+        {
+            if (mqttClient.IsConnected)
+            {
+                Console.WriteLine("Disconnecting MQTT client.");
+                mqttClient.Disconnect();
+            }
+        }
+    }
+}
